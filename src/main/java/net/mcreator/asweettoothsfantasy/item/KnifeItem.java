349e6432168c9757141dd9265e9e2767f54f049d
XeddIsntHere <thepixelswithoutaname@gmail.com>
2021-11-17 18:39:36 -0500

    Added textures and nutritional values for bacon, and textured the bread slice

--- src/main/java/net/mcreator/asweettoothsfantasy/item/KnifeItem.java
+++ src/main/java/net/mcreator/asweettoothsfantasy/item/KnifeItem.java
@@ -3,21 +3,15 @@ package net.mcreator.asweettoothsfantasy.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
-import net.minecraft.world.World;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
-import net.minecraft.client.util.ITooltipFlag;
 
 import net.mcreator.asweettoothsfantasy.itemgroup.ASweetToothsFantasyItemGroup;
 import net.mcreator.asweettoothsfantasy.SweettoothFantasyModElements;
-
-import java.util.List;
 
 @SweettoothFantasyModElements.ModElement.Tag
 public class KnifeItem extends SweettoothFantasyModElements.ModElement {
@@ -72,12 +66,6 @@ public class KnifeItem extends SweettoothFantasyModElements.ModElement {
 			@Override
 			public boolean isRepairable(ItemStack itemstack) {
 				return false;
-			}
-
-			@Override
-			public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
-				super.addInformation(itemstack, world, list, flag);
-				list.add(new StringTextComponent("(You make the texture Isaiah)"));
 			}
 		}.setRegistryName("knife"));
 	}
