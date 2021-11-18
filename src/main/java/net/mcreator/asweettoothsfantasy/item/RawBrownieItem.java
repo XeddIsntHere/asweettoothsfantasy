0d9732f35ae2aac6cb61e2d6ed09623d09883cbb
CrashedGaming <vincr@DESKTOP-8BCUD11>
2021-11-17 23:38:33 -0500

    Added Raw and cooked Brownie.

--- src/main/java/net/mcreator/asweettoothsfantasy/item/RawBrownieItem.java
+++ src/main/java/net/mcreator/asweettoothsfantasy/item/RawBrownieItem.java
@@ -6,11 +6,12 @@ import net.minecraftforge.registries.ObjectHolder;
 import net.minecraft.world.World;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.UseAction;
 import net.minecraft.item.Rarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
+import net.minecraft.item.Food;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.block.BlockState;
 
 import net.mcreator.asweettoothsfantasy.itemgroup.ASweetToothsFantasyItemGroup;
 import net.mcreator.asweettoothsfantasy.SweettoothFantasyModElements;
@@ -18,36 +19,27 @@ import net.mcreator.asweettoothsfantasy.SweettoothFantasyModElements;
 import java.util.List;
 
 @SweettoothFantasyModElements.ModElement.Tag
-public class BrownSugerItem extends SweettoothFantasyModElements.ModElement {
-	@ObjectHolder("sweettooth_fantasy:brown_sugar")
+public class RawBrownieItem extends SweettoothFantasyModElements.ModElement {
+	@ObjectHolder("sweettooth_fantasy:raw_brownie")
 	public static final Item block = null;
-	public BrownSugerItem(SweettoothFantasyModElements instance) {
-		super(instance, 120);
+	public RawBrownieItem(SweettoothFantasyModElements instance) {
+		super(instance, 135);
 	}
 
 	@Override
 	public void initElements() {
-		elements.items.add(() -> new ItemCustom());
+		elements.items.add(() -> new FoodItemCustom());
 	}
-	public static class ItemCustom extends Item {
-		public ItemCustom() {
-			super(new Item.Properties().group(ASweetToothsFantasyItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
-			setRegistryName("brown_sugar");
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ASweetToothsFantasyItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(2).saturation(0.3f).build()));
+			setRegistryName("raw_brownie");
 		}
 
 		@Override
-		public int getItemEnchantability() {
-			return 0;
-		}
-
-		@Override
-		public int getUseDuration(ItemStack itemstack) {
-			return 0;
-		}
-
-		@Override
-		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
-			return 1F;
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
 		}
 
 		@Override
