77c0361970120388185f14624e56fb02fa6bd2a1
CrashedGaming <vincr@192.168.1.42>
2022-01-15 13:02:23 -0500

    Updated the  description

--- src/main/java/net/mcreator/asweettoothsfantasy/item/CookedFrenchToastItem.java
+++ src/main/java/net/mcreator/asweettoothsfantasy/item/CookedFrenchToastItem.java
@@ -3,20 +3,14 @@ package net.mcreator.asweettoothsfantasy.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
-import net.minecraft.world.World;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraft.item.UseAction;
 import net.minecraft.item.Rarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.Food;
-import net.minecraft.client.util.ITooltipFlag;
 
 import net.mcreator.asweettoothsfantasy.itemgroup.ASweetToothsFantasyItemGroup;
 import net.mcreator.asweettoothsfantasy.SweettoothFantasyModElements;
-
-import java.util.List;
 
 @SweettoothFantasyModElements.ModElement.Tag
 public class CookedFrenchToastItem extends SweettoothFantasyModElements.ModElement {
@@ -40,12 +34,6 @@ public class CookedFrenchToastItem extends SweettoothFantasyModElements.ModEleme
 		@Override
 		public UseAction getUseAction(ItemStack itemstack) {
 			return UseAction.EAT;
-		}
-
-		@Override
-		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
-			super.addInformation(itemstack, world, list, flag);
-			list.add(new StringTextComponent("(You make the texture Isaiah)"));
 		}
 	}
 }
