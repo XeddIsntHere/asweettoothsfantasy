2ca9b5202194d067fc59473bee1868b51eee8e75
CrashedGaming <vincr@192.168.1.42>
2022-01-15 13:51:22 -0500

    Added mug and mug recipe.

---
+++ src/main/java/net/mcreator/asweettoothsfantasy/item/MugItem.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.asweettoothsfantasy.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.asweettoothsfantasy.itemgroup.ASweetToothsFantasyItemGroup;
+import net.mcreator.asweettoothsfantasy.SweettoothFantasyModElements;
+
+import java.util.List;
+
+@SweettoothFantasyModElements.ModElement.Tag
+public class MugItem extends SweettoothFantasyModElements.ModElement {
+	@ObjectHolder("sweettooth_fantasy:mug")
+	public static final Item block = null;
+	public MugItem(SweettoothFantasyModElements instance) {
+		super(instance, 140);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ASweetToothsFantasyItemGroup.tab).maxStackSize(1).rarity(Rarity.COMMON));
+			setRegistryName("mug");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("(You make the texture Isaiah)"));
+		}
+	}
+}
